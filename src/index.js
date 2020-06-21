f8334fe7e92e45a88a619ea1a597d698b7c9d32f
klsdklsdkl <lkasklsa@klalkas.com>
2020-06-20 19:54:12 -0600

    Try to deploy 5

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "assets/vendors/vendors.min.css";
+import "assets/css/themes/vertical-dark-menu-template/style.css";
+
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
