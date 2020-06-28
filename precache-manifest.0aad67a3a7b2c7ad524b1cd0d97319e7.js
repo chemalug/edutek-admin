3c82beb90e9c0d393c74ff049b7bec068c46d1ae
klsdklsdkl <lkasklsa@klalkas.com>
2020-06-28 11:38:57 -0600

    Updates

---
+++ precache-manifest.0aad67a3a7b2c7ad524b1cd0d97319e7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65fbd37bcdebd40a42f20f78d2f3db46",
+    "url": "/edutek-admin/index.html"
+  },
+  {
+    "revision": "a15a0aba8a3a5be2b926",
+    "url": "/edutek-admin/static/css/main.9a276008.chunk.css"
+  },
+  {
+    "revision": "0fb33c63653f1ca1c529",
+    "url": "/edutek-admin/static/js/2.9df05fbf.chunk.js"
+  },
+  {
+    "revision": "80863c161c46f0ebc8be59a57cdc2f00",
+    "url": "/edutek-admin/static/js/2.9df05fbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a15a0aba8a3a5be2b926",
+    "url": "/edutek-admin/static/js/main.7d9cfed3.chunk.js"
+  },
+  {
+    "revision": "bc102a919208609dc346",
+    "url": "/edutek-admin/static/js/runtime-main.33f797a7.js"
+  },
+  {
+    "revision": "a8acab001a7c2bf3af7bcd32fc1a0e19",
+    "url": "/edutek-admin/static/media/3.a8acab00.png"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/edutek-admin/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/edutek-admin/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/edutek-admin/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/edutek-admin/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "5d2183902dbaa305529428e69b923cf3",
+    "url": "/edutek-admin/static/media/applewatch.5d218390.png"
+  },
+  {
+    "revision": "93a42620615476fd9398831874d6c2b3",
+    "url": "/edutek-admin/static/media/assigment.93a42620.png"
+  },
+  {
+    "revision": "22fb5b7fb6b6234fbf05e933ba8b0fcf",
+    "url": "/edutek-admin/static/media/avatar-7.22fb5b7f.png"
+  },
+  {
+    "revision": "59aa18eab53aeb478d6acc30fc1436ac",
+    "url": "/edutek-admin/static/media/breadcrumb-bg.59aa18ea.jpg"
+  },
+  {
+    "revision": "548eb8f3fbc4e1b774f1cb484c0d3c0a",
+    "url": "/edutek-admin/static/media/flat-bg.548eb8f3.jpg"
+  },
+  {
+    "revision": "9352a4eb3d01d0d0e3962173153750d5",
+    "url": "/edutek-admin/static/media/iphone.9352a4eb.png"
+  },
+  {
+    "revision": "2d07ebdacda7cb371f18925edcf69a54",
+    "url": "/edutek-admin/static/media/macbook.2d07ebda.png"
+  },
+  {
+    "revision": "54ea9e69985f0689da47424e748e3fd7",
+    "url": "/edutek-admin/static/media/professor.54ea9e69.png"
+  }
+]);
